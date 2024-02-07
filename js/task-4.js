3682d93e1998d08049571be527f9286b29d48821
imyroslav <imyroslav@ukr.net>
2024-02-07 17:09:26 +0700

    task-4-complete

---
+++ js/task-4.js
@@ -0,0 +1,18 @@
+const loginForm = document.querySelector(".login-form");
+
+loginForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (email === "" || password === "") {
+    return alert("All form fields must be filled in");
+  }
+ 
+
+  console.log(`Email: ${email}, Password: ${password}`);
+  form.reset();}
+
