ea40aa242bb1b7e4382f0436e68284f4f47060e9
imyroslav <imyroslav@ukr.net>
2024-02-08 23:55:04 +0700

    task-6-completed

--- js/task-6.js
+++ js/task-6.js
@@ -2,5 +2,38 @@ function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215)
       .toString(16)
       .padStart(6, 0)}`;
+}
+
+function clearInput() {
+  let getValue= document.querySelector("input");
+    if (getValue.value !="") {
+        getValue.value = "";
+    }
+}
+
+
+const divDad = document.querySelector("#boxes");
+const input = document.querySelector("input");
+const createBtn = document.querySelector('button[data-create]');
+const destroyBtn = document.querySelector('button[data-destroy]');
+let side = 20
+
+createBtn.addEventListener("click", (event) => {
+  const amount = input.value
+  if (amount >=1 && amount <= 100) {
+    for( let i = 1; i <= amount; i += 1) {
+      const divElem = document.createElement("div");
+      side = side + 10;
+      divElem.style.width = String(`${side}px`);
+      divElem.style.height = String(`${side}px`);
+      divElem.style.backgroundColor = getRandomHexColor();
+      divDad.append(divElem);
+    }
   }
-  
+  clearInput();
+}) 
+
+destroyBtn.addEventListener("click", (event) => {
+  const boxeS = divDad.querySelector("div");
+  boxeS.remove();
+})
