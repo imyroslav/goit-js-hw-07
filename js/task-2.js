1bc6c1a9b9f948d6a497d593d05edbc6fb4f7140
imyroslav <imyroslav@ukr.net>
2024-02-14 00:05:27 +0700

    task-2-correction

--- js/task-2.js
+++ js/task-2.js
@@ -1,48 +1,46 @@
 const images = [
-    {
-      url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'White and Black Long Fur Cat',
-    },
-    {
-      url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    },
-    {
-      url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      alt: 'Group of Horses Running',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      alt: 'Elephant Beside on Baby Elephant',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      alt: 'Blue Geeen and Orange Parrot',
-    },
-    {
-      url:
-        'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-      alt: 'Zebras on Zebra',
-    },
-  ];
-  
-  const list = document.querySelector(".gallery");
-  
-  for (const image of images) {
-    const listEl = document.createElement("li");
-    list.append(listEl);
-    const imgEl = document.createElement("img");
-    imgEl.setAttribute("src", image.url);
-    imgEl.setAttribute("alt", image.alt);
-    imgEl.setAttribute("width", "360");
-    listEl.append(imgEl);
-  }
-  
-  list.style.display = "flex";
-  list.style.flexDirection = "row";
-  list.style.flexWrap = "wrap";
+  {
+    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'White and Black Long Fur Cat',
+  },
+  {
+    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+  },
+  {
+    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    alt: 'Group of Horses Running',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Elephant Beside on Baby Elephant',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Blue Geeen and Orange Parrot',
+  },
+  {
+    url:
+      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    alt: 'Zebras on Zebra',
+  },
+];
+
+const list = document.querySelector(".gallery");
+
+const markup = images
+.map((image) => `<li>
+<img src="${image.url}" alt="${image.alt}" width="360"> </li>`)
+.join("");
+
+list.insertAdjacentHTML("beforeend", markup);
+
+
+list.style.display = "flex";
+list.style.flexDirection = "row";
+list.style.flexWrap = "wrap";
   
   
   
