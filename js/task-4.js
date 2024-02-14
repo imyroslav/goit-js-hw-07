7d52edbfdb785240e233c6b623fe28f551ffb102
imyroslav <imyroslav@ukr.net>
2024-02-15 02:12:36 +0700

    task-6=form-rest

--- js/task-4.js
+++ js/task-4.js
@@ -4,19 +4,18 @@ loginForm.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   event.preventDefault();
-  const form = event.target;
-  const email = form.elements.email.value;
-  const password = form.elements.password.value;
-  const formData = {
-    Email: "email",
-    Password: "password",
-  }
+  const email = event.target.elements.email.value;
+  const password = event.target.elements.password.value;
+
   
   if (email === "" || password === "") {
     alert("All form fields must be filled in");
   } else { 
-    formData.Email = email;
-    formData.Password = password;
+    const formData = {
+      email: email.trim(),
+      password: password.trim(),
+    }
     console.log(formData);
   }
+  loginForm.reset();
   }
